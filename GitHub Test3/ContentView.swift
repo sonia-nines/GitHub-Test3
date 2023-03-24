60709a20af963718d9c4aeff43d7b285823fa701
Sonia <sonia.nines@gmail.com>
2023-03-24 13:35:04 +0900

    Initial Commit

---
+++ GitHub Test3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitHub Test3
+//
+//  Created by Min Gyo P. on 2023/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHub Test3/GitHub_Test3App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_Test3App.swift
+//  GitHub Test3
+//
+//  Created by Min Gyo P. on 2023/03/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_Test3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
